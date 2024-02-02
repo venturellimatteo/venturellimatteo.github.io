ed664f0d79b45cedf6459e228b28419428d9569b
Matteo Venturelli <matteo.venturelli2000@gmail.com>
2024-02-02 10:35:55 +0100

    jquery implementation

---
+++ src/layout-init.js
@@ -0,0 +1,12 @@
+AOS.init();
+$(document).ready(function() {
+  var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  gutter: '.gutter-sizer',
+  });
+
+  $grid.imagesLoaded().progress(function() {
+  $grid.masonry('layout');
+  });
+});
+  
